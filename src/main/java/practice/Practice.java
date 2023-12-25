80ab03c06768d0620001669fff455bb01f96c805
HPRN\ryuga <jprnwork@gmail.com>
2023-12-25 15:26:31 +0900

    update

--- src/main/java/practice/Practice.java
+++ src/main/java/practice/Practice.java
@@ -5,15 +5,15 @@ public class Practice {
 	public static void main(String[] args) {
 
 		PracticeWhile.practiceWhile();
-		
+
 		PracticeDoWhile.practiceDoWhile();
-		
+
 		PracticeFor.practiceFor();
-		
+
 		PracticeForEach.practiceForEach();
-		
+
 		PracticeIf.practiceIf();
-		
+
 		PracticeSwitch.practiceSwitch();
 
 	}
--- src/main/java/practice/PracticeFor.java
+++ src/main/java/practice/PracticeFor.java
@@ -3,16 +3,16 @@ package practice;
 public class PracticeFor {
 
 	public static void practiceFor() {
-		
-		int sum = 0; //総和の初期値を0に設定
-		
-		for (int i = 1; i <= 100; i++) { //iは1に設定, iは100以下に設定, 1づつ加算
-			
-			sum += i; //sum + iの値を加算
+
+		int sum = 0; // 総和の初期値を0に設定
+
+		for (int i = 1; i <= 100; i++) { // iは1に設定, iは100以下に設定, 1づつ加算
+
+			sum += i; // sum + iの値を加算
 		}
-		
+
 		System.out.println("1から100までの整数を足した総和：" + sum);
-		//forループした結果の総和を表示
+		// forループした結果の総和を表示
 	}
 
 }
